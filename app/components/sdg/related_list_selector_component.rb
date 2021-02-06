8c248a5906c90907ccb44f476051a805a7ddca49
Javi Martín <javim@elretirao.net>
2021-02-06 20:03:37 +0100

    Use real check boxes to select goals
    We were using list items with the checkbox role. This is probably
    confusing since list items have a listitem role, and so we were
    basically using a list with no listitem.
    
    We could add a `<span role="checkbox">` tag inside the list item, but
    using real checkboxes is probably easier.
    
    Note we're using the "enter" key to toggle the "checked" status of the
    SDG. This is probably not intuitive for screen reader users who might
    try to submit the form using the "enter" key after selecting a goal.
    
    However, the alternative would be even less intuitive for sighted
    keyboard users, since for them these icons look like links or buttons
    and they would accidentally submit the form when trying to select a
    goal.
    
    Since we haven't come up with a better interface yet, for now we're
    following the principle of least damage; we consider submitting the form
    against a user's will is less annoying than forcing users to move to a
    different field before being able to submit the form.
    
    Also note we can't write `check` in the tests because Capybara will try
    to click the checkbox, which is hidden by the image in the label.

--- app/components/sdg/related_list_selector_component.rb
+++ app/components/sdg/related_list_selector_component.rb
@@ -39,6 +39,13 @@ class SDG::RelatedListSelectorComponent < ApplicationComponent
       SDG::Goal.order(:code)
     end
 
+    def goal_field(checkbox_form)
+      goal = checkbox_form.object
+
+      checkbox_form.check_box(data: { code: goal.code }) +
+        checkbox_form.label { render(SDG::Goals::IconComponent.new(goal)) }
+    end
+
     def text_for(goal_or_target)
       if goal_or_target.class.name == "SDG::Goal"
         t("sdg.related_list_selector.goal_identifier", code: goal_or_target.code)
