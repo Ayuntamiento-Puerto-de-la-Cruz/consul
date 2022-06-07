2b7b0a88cbe26e2c9149b67e48bb48a20b98136b
Alberto Clavijo <albert.cla@gmail.com>
2022-06-07 15:50:35 +0100

    Add customs to helpers

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -2,5 +2,6 @@ module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :es
     config.i18n.available_locales = [:es]
+    config.paths["app/helpers"].unshift(Rails.root.join("app", "helpers", "custom"))
   end
 end
