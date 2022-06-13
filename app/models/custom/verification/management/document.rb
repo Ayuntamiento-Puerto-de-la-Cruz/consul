78af2023d0b7758e7c91456a09a8806871443042
Alberto Clavijo <albert.cla@gmail.com>
2022-06-13 12:39:09 +0100

    Update custom verification management document to use date_of_birth from the form instead from the response

--- app/models/custom/verification/management/document.rb
+++ app/models/custom/verification/management/document.rb
@@ -1,8 +1,7 @@
 require_dependency Rails.root.join("app", "models", "verification", "management", "document").to_s
 
 class Verification::Management::Document
-  def in_census?
-    response = CensusCaller.new.call(document_type, document_number, date_of_birth, postal_code)
-    response.valid?
+  def under_age?(response)
+    response.date_of_birth.blank? || Age.in_years(date_of_birth) < User.minimum_required_age
   end
 end
