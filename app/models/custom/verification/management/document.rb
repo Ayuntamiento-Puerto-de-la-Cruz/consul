5c8d742ffa4b43428ef97d745296fc80c22c0d7b
Alberto Clavijo <albert.cla@gmail.com>
2022-06-16 09:45:06 +0100

    Add a comment explaining why to use a custom under_age method within the model verification management document

--- app/models/custom/verification/management/document.rb
+++ app/models/custom/verification/management/document.rb
@@ -2,6 +2,7 @@ require_dependency Rails.root.join("app", "models", "verification", "management"
 
 class Verification::Management::Document
   def under_age?(response)
+    # There is no data in the response from the census, so we use the age that the user input
     Age.in_years(date_of_birth) < User.minimum_required_age
   end
 end
