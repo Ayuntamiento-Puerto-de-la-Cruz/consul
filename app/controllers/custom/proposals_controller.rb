2cc05924490d9b2088d8846af35f5dbafb412f74
Alejandro Vera <alejandro.vera1988@gmail.com>
2025-04-28 11:57:34 +0100

    feat: Add SUA to proposal

---
+++ app/controllers/custom/proposals_controller.rb
@@ -0,0 +1,16 @@
+require_dependency Rails.root.join("app", "controllers", "proposals_controller").to_s
+
+class ProposalsController
+  private
+
+    def proposal_params
+      attributes = [:video_url, :responsible_name, :tag_list, :terms_of_service,
+                    :geozone_id, :skip_map, :related_sdg_list, :related_sua_list,
+                    image_attributes: image_attributes,
+                    documents_attributes: [:id, :title, :attachment, :cached_attachment,
+                                           :user_id, :_destroy],
+                    map_location_attributes: [:latitude, :longitude, :zoom]]
+      translations_attributes = translation_params(Proposal, except: :retired_explanation)
+      params.require(:proposal).permit(attributes, translations_attributes)
+    end
+end
