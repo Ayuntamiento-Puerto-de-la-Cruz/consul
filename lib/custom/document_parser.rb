14c2f5623a6b34ec0016bacce88de194edb39916
Alberto Clavijo <albert.cla@gmail.com>
2025-09-03 11:34:40 +0100

    fix: Write self.class again

--- lib/custom/document_parser.rb
+++ lib/custom/document_parser.rb
@@ -2,7 +2,7 @@ require_dependency Rails.root.join("lib", "document_parser").to_s
 
 module DocumentParser
   def dni?(document_type)
-    dni_type = class.name == "RemoteCensusApi" ? "D" : "1"
+    dni_type = self.class.name == "RemoteCensusApi" ? "D" : "1"
     document_type.to_s == dni_type
   end
 end
